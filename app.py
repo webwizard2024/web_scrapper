fa1cef8d36d2837ed84675f21825e1888fcafafd
Ayesha_Batool <160107340+webwizard2024@users.noreply.github.com>
2025-10-29 11:25:18 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -6,7 +6,8 @@ from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_community.vectorstores import FAISS
 from langchain_community.embeddings import HuggingFaceEmbeddings
 from langchain_groq import ChatGroq
-from langchain.chains import ConversationalRetrievalChain
+from langchain_community.chains import ConversationalRetrievalChain
+
 
 # Ignore unnecessary warnings
 warnings.filterwarnings("ignore")
